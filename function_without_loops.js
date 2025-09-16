2e594aaa7855dac83c931d118efbc64dcc02834a
Prashchuk Danylo <prashchukdanylo@gmail.com>
2025-09-16 16:45:29 +0300

    Initial commit

---
+++ function_without_loops.js
@@ -0,0 +1,22 @@
+//Середнє арифметичне 2 чисел//
+const average = (a, b) => {
+  console.log((a+b)/2);
+}
+
+average(1,6);
+
+//Квадрат числа//
+const square = (x) => {
+  x = x*x;
+  console.log(x);
+}
+
+square(5);
+
+//Куб числа//
+const cube = (y) => {
+  y = y*y*y;
+  console.log(y);
+}
+
+cube(-3);
