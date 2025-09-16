6b326f8673c033975ed5fb76a536390798e92e03
Prashchuk Danylo <prashchukdanylo@gmail.com>
2025-09-16 15:51:02 +0300

    Initial commit

---
+++ identificators.js
@@ -0,0 +1,13 @@
+var myName = "Danylo";
+console.log(typeof myName);
+
+
+const birthDate = 2008;
+console.log(typeof birthDate);
+
+
+const greeting = (name) => {
+  console.log(`Hello, ${name}!`);
+}
+
+greeting('Petro');
