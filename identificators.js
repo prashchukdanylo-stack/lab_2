a1f6728b16fd64bb58e848fdc732445c6c6eb63f
Prashchuk Danylo <prashchukdanylo@gmail.com>
2025-09-16 16:49:11 +0300

    Added comments

--- identificators.js
+++ identificators.js
@@ -1,11 +1,12 @@
+//Змінна ім'я//
 var myName = "Danylo";
 console.log(typeof myName);
 
-
+//Константа рік народження//
 const birthDate = 2008;
 console.log(typeof birthDate);
 
-
+//Функція привітання з вказаним ім'ям//
 const greeting = (name) => {
   console.log(`Hello, ${name}!`);
 }
